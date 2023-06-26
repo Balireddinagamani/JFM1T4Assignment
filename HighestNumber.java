84dabdbfc423e36e4a4b4842bd2dc1d0b3b449bf
Balireddinagamani <133116243+Balireddinagamani@users.noreply.github.com>
2023-06-26 20:23:14 +0530

    Add files via upload

---
+++ HighestNumber.java
@@ -0,0 +1,49 @@
+/* JFM1T4_Assignment1:
+   Write a program to find the highest among the given 3 numbers.
+   Prompt the user for the 3 numbers to be input from the terminal.
+
+   SampleInput: 
+   Enter the first number: 11
+   Enter the second number: 22
+   Enter the third number: 1
+
+   Expected Output:
+   22 is highest number
+ 	
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.Scanner;
+
+public class HighestNumber {
+     
+//Add main method
+public static void main(String[]args){
+//Declare the three variables
+int num1,num2,num3;
+//Use the scanner class to provide input at execution time
+/* 
+ Scanner s= new Scanner(System.in);
+ System.out.println("Enter the first number");
+ number1=s.nextInt();
+
+*/
+Scanner sc = new Scanner(System.in);
+  System.out.println("Enter the first number");
+  int a = sc.nextInt();
+  int b = sc.nextInt();
+  int c = sc.nextInt();
+//check which number is highest using if else condition
+if(a>b && a>c )
+  System.out.println("first number is highest");
+  else if(b>c && b>a)
+    System.out.println("second number is highest");
+  else 
+    System.out.println("third number is highest");
+  
+
+    
+}
+
+ 
+}
---
+++ PrimeNumbers.java
@@ -0,0 +1,47 @@
+/* JFM1T4_Assignment3:
+   Write a program to print all the prime numbers between 2 numbers.
+   Prompt the user to enter the 2 numbers.
+   
+   Sample Input:
+   Enter starting Number: 20
+   Enter ending Number: 30
+
+   Expected Output:
+   Prime numbers between 20 and 30 are : 
+   23
+   29
+
+*/
+
+//import statements for java program to read inputs using Scanner class 
+import java.util.Scanner;
+
+public class PrimeNumbers {
+  public static void main(String[]args){
+    int i,count;
+  Scanner sc = new Scanner(System.in);
+  System.out.print("Enter the two numbers:");
+    int n=sc.nextInt();
+    System.out.println("prime number");
+    for(int j=2;j<=n;j++)
+      {
+        count=0;
+        for(i=1;i<=j;i++)
+          {
+            if(j%i==0)
+            {
+              count++;
+            }
+            }
+        if(count==2)
+          System.out.println(j+" ");
+            
+   
+   
+
+              
+    }
+              
+        
+  }
+}
